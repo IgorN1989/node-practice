ef501eb23d26128e75cf8463e64e87ec9eb779e5
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2024-01-08 22:22:09 +0200

    lesson-8

---
+++ LESSON-8/server.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+// import { DB_HOST } from "./config.js";
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
